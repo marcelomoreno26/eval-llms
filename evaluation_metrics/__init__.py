32e23ae8df026f581bc467d291625d4cf4354c3c
marcelomoreno26 <morenomarce9@gmail.com>
2025-01-28 00:07:21 +0100

    add code for prometheus score

--- evaluation_metrics/__init__.py
+++ evaluation_metrics/__init__.py
@@ -2,3 +2,4 @@ from .biencoder import BiEncoderScore
 from .sas import SemanticAnswerSimilarity
 from .sas_ensemble import SASEnsemble
 from .accuracy import Accuracy
+from .prometheus import PrometheusScore
