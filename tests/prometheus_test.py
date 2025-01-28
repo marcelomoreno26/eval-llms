3d79284c286a8310660b4f5f277a38ef59bb51cc
marcelomoreno26 <morenomarce9@gmail.com>
2025-01-28 20:41:15 +0100

    update tests

--- tests/prometheus_test.py
+++ tests/prometheus_test.py
@@ -3,23 +3,97 @@ from evaluation_metrics import PrometheusScore
 
 model_name = "meta-llama/Llama-3.1-8B-Instruct"
 
+
 predictions = [
-        "El sol brilla debido a reacciones de fusión nuclear en su núcleo.",
-        "La capital de Francia es París."
-    ]
+    "El sol brilla debido a reacciones de fusión nuclear en su núcleo.",
+    "La capital de Francia es París."
+]
 
 references = [
     "El sol produce luz y calor mediante reacciones de fusión nuclear en su núcleo.",
     "París es la capital de Francia."
-    ]
+]
+
 
 contexts = [
     "El Sol emite energía en forma de luz y calor, generada a través de reacciones de fusión nuclear en su núcleo.",
     "Francia es un país europeo cuya capital es París."
+]
+
+
+previous_conversations = [
+    [
+        {"role": "user", "content": "¿Qué es una estrella?"},
+        {"role": "assistant", "content": "Una estrella es una esfera de plasma, principalmente hidrógeno y helio, que emite luz y calor debido a las reacciones de fusión nuclear en su núcleo."},
+        {"role": "user", "content": "¿Por qué el Sol brilla?"}
+    ],
+    [
+        {"role": "user", "content": "¿Qué es Francia?"},
+        {"role": "assistant", "content": "Francia es un país europeo conocido por su historia, su cultura y su gastronomía. Su capital es París."},
+        {"role": "user", "content": "¿Cuáles son algunos de los monumentos famosos de Francia?"}
     ]
+]
 
-metric = PrometheusScore()
-score = metric.compute(model_name=model_name, predictions=predictions, references=references, contexts=contexts)
 
-assert isinstance(score, float)
-assert 1 <= score <= 10
+
+def test_default():
+    metric = PrometheusScore()
+    scores = metric.compute(model_name=model_name, predictions=predictions, references=references, contexts=contexts, previous_conversations=previous_conversations)
+    
+    assert isinstance(scores, list)
+    assert all(isinstance(score, int) for score in scores)
+
+
+
+def test_no_previous_conversation():
+    metric = PrometheusScore()
+    scores = metric.compute(model_name=model_name, predictions=predictions, references=references, contexts=contexts)
+    
+    assert isinstance(scores, list)
+    assert all(isinstance(score, int) for score in scores)
+
+
+
+def test_return_average():
+    metric = PrometheusScore()
+    scores, avg_score = metric.compute(model_name=model_name, predictions=predictions, references=references, contexts=contexts, previous_conversations=previous_conversations, return_average=True)
+
+    assert isinstance(scores, list)
+    assert all(isinstance(score, int) for score in scores)
+    assert isinstance(avg_score, float)
+    assert 1 <= avg_score <= 10
+
+
+
+def test_return_feedbacks():
+    metric = PrometheusScore()
+    scores, feedbacks = metric.compute(model_name=model_name, predictions=predictions, references=references, contexts=contexts, previous_conversations=previous_conversations, return_average=True)
+
+    assert isinstance(scores, list)
+    assert all(isinstance(score, int) for score in scores)
+    assert isinstance(feedbacks, list)
+    assert all(isinstance(feedback, str) for feedback in feedbacks)
+
+
+
+def test_return_all():
+    metric = PrometheusScore()
+    scores, feedbacks, avg_score = metric.compute(model_name=model_name, predictions=predictions, references=references, contexts=contexts, previous_conversations=previous_conversations, return_feedbacks=True, return_average=True)
+
+    assert isinstance(scores, list)
+    assert all(isinstance(score, int) for score in scores)
+    assert isinstance(feedbacks, list)
+    assert all(isinstance(feedback, str) for feedback in feedbacks)
+    assert isinstance(avg_score, float)
+    assert 1 <= avg_score <= 10
+
+
+
+if __name__ == "__main__":
+    test_default()
+    test_no_previous_conversation()
+    test_return_average()
+    test_return_feedbacks()
+    test_return_all()
+
+    print("All tests passed for Prometheus Score!")
--- tests/sas_test.py
+++ tests/sas_test.py
@@ -1,11 +1,12 @@
-from sentence_transformers import CrossEncoder
 from evaluation_metrics import SemanticAnswerSimilarity
+
 
 references = [
     "El sol brilla en el cielo.",
     "Las bicicletas son ecológicas.",
     "El café es una bebida popular."
 ]
+
 
 predictions = [
     "El sol está en el cielo.",
@@ -14,7 +15,39 @@ predictions = [
 ]
 
 
-metric = SemanticAnswerSimilarity()
-score = metric.compute(predictions=predictions, references=references, batch_size=4)
-assert isinstance(score, float)
-assert 0 <= score <= 1
+
+def test_default():
+    metric = SemanticAnswerSimilarity()
+    scores = metric.compute(predictions=predictions, references=references)
+    
+    assert isinstance(scores, list)
+    assert all(isinstance(score, float) for score in scores)
+
+
+
+def test_return_average():
+    metric = SemanticAnswerSimilarity()
+    scores, avg_score = metric.compute(predictions=predictions, references=references, return_average=True)
+
+    assert isinstance(scores, list)
+    assert all(isinstance(score, float) for score in scores)
+    assert isinstance(avg_score, float)
+    assert 0 <= avg_score <= 1
+
+
+
+def test_invalid_model():
+    metric = SemanticAnswerSimilarity()
+    scores = metric.compute(model_name="sentence-transformers/paraphrase-multilingual-mpnet-base-v2",predictions=predictions, references=references)
+
+    assert scores is None
+
+
+
+if __name__ == "__main__":
+    test_default()
+    test_return_average()
+    test_invalid_model()
+
+    print("All tests passed for Semantic Answer Similarity!")
+
